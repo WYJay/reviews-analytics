6fa26b3edbcf1006e3aaf192ff563ba1ffce5415
WJay <alexchris8888@gmail.com>
2022-08-25 23:09:55 +0800

    cal avg reviews

--- review_analytics.py
+++ review_analytics.py
@@ -6,6 +6,9 @@ with open ('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 ==0:
 			print(len(data))
-print(data[0])
-print('-------------')
-print(data[1])
+print('檔案讀取完,總共有', len(data), '比資料')
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	
+print('留言平均長度是', sum_len/len(data))
